a213db77390e0f0a277ea6ab3f62b996cf1c59a0
Doug <>
2014-09-04 11:43:37 -0600

    adding type parsing

--- js/flat.js
+++ js/flat.js
@@ -8,10 +8,35 @@ var Flat = (function(){
       var keyvals = item.split("\n");
       for(var j = 0; j < keyvals.length; j++){
         var keyvalSplit = keyvals[i].split(":");
-        itemObj[keyvalSplit[0]] = keyvalSplit[1];
+        itemObj[keyvalSplit[0]] = getValue(keyvalSplit[1]);
       }
       itemsArr.push(itemObj);
     }
     return itemsArr;
   }
+  function getValue(text){
+    //array
+    if(text.indexOf(",") != -1){
+      var textValues = text.split(",");
+      var arr = [];
+      for(var i = 0; i < values.length; i++){
+        arr.push(getValue(textValues[i]))
+      }
+      return arr;
+    }
+    //date
+    var date = new Date(text);
+    if(date.getTime() === date.getTime()){
+      return date;
+    }
+    //number
+    if(!isNaN(text)){
+      return parseInt(text);
+    }
+    //string
+    return text;
+  }
+  return {
+    parse : parse
+  };
 })();
